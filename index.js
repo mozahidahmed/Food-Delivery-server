af098b5f9f6d41ed56542dacf11b1d4380b6e9c8
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-04-21 20:29:42 +0600

    database connect

--- index.js
+++ index.js
@@ -11,10 +11,8 @@ app.use(express.json());
 
 
 //connection database
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ro517.mongodb.net/?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://service:WqppylUxViB54LVR@cluster0.ro517.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
 
 
 
@@ -23,7 +21,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const serviceCollection = client.db('').collection('');
+        const serviceCollection = client.db('pandami').collection('rasturent');
         
     }
 
