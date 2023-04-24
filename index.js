7e65d5889b04df7e5b8a72aeb43d4d96665fc67d
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-04-24 13:53:57 +0600

    get data with id

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
+
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -34,11 +35,12 @@ async function run() {
         
         app.get('/restaurants/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId(id) }
-            const restaurant = await restaurantsCollection.findOne(query);
-            res.send(restaurant)
+            const query={_id: new ObjectId(id)}
+            const restaurants = await restaurantsCollection.findOne(query);
+            res.send(restaurants);
         })
 
+     
 
 
 
